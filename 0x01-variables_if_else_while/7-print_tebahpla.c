c70e8478e4fb3514dabdc630be95a7189fff2d89
martnm <musili29@gmail.com>
2022-10-20 06:24:28 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+*main - print the latters of the alphabet
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+
+	return (0);
+}

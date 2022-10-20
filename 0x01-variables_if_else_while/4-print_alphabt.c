320f95fea716d250452cfd5ae0fab3e13d496d58
martnm <musili29@gmail.com>
2022-10-20 06:05:25 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet
+ * Description: print the letters of the alphabet except e, q
+ * Return: Success
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
